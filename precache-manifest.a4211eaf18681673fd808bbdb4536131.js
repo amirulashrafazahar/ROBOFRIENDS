cbb642d8465a613812cb0928821c448ce31ce443
Amirul <amirulict@gmail.com>
2022-09-24 17:30:16 +0800

    Updates

---
+++ precache-manifest.a4211eaf18681673fd808bbdb4536131.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "55cd572e60d2e109831f",
+    "url": "/ROBOFRIENDS/static/css/main.d4947cee.chunk.css"
+  },
+  {
+    "revision": "55cd572e60d2e109831f",
+    "url": "/ROBOFRIENDS/static/js/main.f843dad5.chunk.js"
+  },
+  {
+    "revision": "2d1b03b67b6e341ca880",
+    "url": "/ROBOFRIENDS/static/js/runtime~main.e6647f1b.js"
+  },
+  {
+    "revision": "d769a94945497ed8f1ef",
+    "url": "/ROBOFRIENDS/static/css/2.841f1167.chunk.css"
+  },
+  {
+    "revision": "d769a94945497ed8f1ef",
+    "url": "/ROBOFRIENDS/static/js/2.74959997.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/ROBOFRIENDS/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ROBOFRIENDS/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "104c10a06fe3124a3ceff3e960b0ee30",
+    "url": "/ROBOFRIENDS/index.html"
+  }
+];
